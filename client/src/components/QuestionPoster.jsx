97f49b14cb45649937d71e5caa54a31e966c43b4
Prakhar Mishra <mprakhar35@gmail.com>
2022-04-30 10:28:43 +0000

    commit_latest

--- client/src/components/QuestionPoster.jsx
+++ client/src/components/QuestionPoster.jsx
@@ -2,19 +2,18 @@ import React from 'react'
 
 const QuestionPoster = () => {
   return (
-    <div className="m-3 p-2 shadow-lg rounded-md">
-      <form className="flex flex-col">
+    <div className="m-3 p-2 ">
+      <form className="flex flex-col space-y-2 w-2/5 shadow-lg rounded-md p-3">
         <label for="Sname">Student Name</label>
-        <br/>
         <input placeholder="Student Name" type="text" id="Sname"/>
 
-        <label for="Trade">Student Name</label>
-        <br/>
-        <input placeholder="Student Name" type="text" id="Trade"/>
+        <label for="Trade">Student Trade</label>
+        
+        <input placeholder="Trade" type="text" id="Trade"/>
 
-        <label for="RegnNo">Student Name</label>
-        <br/>
-        <input placeholder="Student Name" type="text" id="RegnNo"/>
+        <label for="RegnNo">Student RegnNo.</label>
+        
+        <input placeholder="RegnNo" type="text" id="RegnNo"/>
 
         <input type="submit" value="Submit" />
       </form>
